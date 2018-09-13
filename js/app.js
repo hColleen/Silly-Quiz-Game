b074ec2691df2b4ef266eb06e4cf670ffa964bf4
hColleen <hcolleen@gmail.com>
2018-09-12 17:04:27 -0700

    set up question/answer array

---
+++ js/app.js
@@ -0,0 +1,129 @@
+document.getElementById('question').innerHTML = "Hello, Seeker";
+document.getElementById('answer').innerHTML = "What do you want to know?";
+
+let questionsAnswers = [{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+    },{
+    question: '',
+    answer1: '',
+    answer2: '',
+    answer3: ''
+}];
