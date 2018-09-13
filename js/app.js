7ac1adb3f195a82fed0babb061795636bf017b12
hColleen <hcolleen@gmail.com>
2018-09-13 02:39:05 -0700

    create quiz question array from full array

--- js/app.js
+++ js/app.js
@@ -127,3 +127,37 @@ let questionsAnswers = [{
     answer2: '',
     answer3: ''
 }];
+
+//select random questions/answers from array, preventing repeats (tutorial from here: https://inteist.com/javascript-generate-pseudo-random-set/ )
+
+function numArr(limit){
+	list = [];
+	for (let i = 0; i < limit; i ++){
+		list.push(i);
+	}
+	return list;
+}
+
+function shuffle(array){
+	let i = array.length,
+		j = 0,
+		temp;
+	while (i --){
+		j = Math.floor(Math.random() * (i + 1))
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	return array;
+}
+
+let getRandomQuestion = shuffle(numArr(questionsAnswers.length));
+
+let quizArray = [], quizLength = 5;
+
+function createQuzArray() {
+    for (let i = 0; i < quizLength; i++){
+        let quest = questionsAnswers[getRandomQuestion[i]];
+        quizArray.push(quest);
+    }
+}
